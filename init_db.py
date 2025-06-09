2305763c69680f29e9f93fce776852bd010b1b42
Matthew <74075600+mlafantaisie@users.noreply.github.com>
2025-06-09 13:22:38 -0400

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,6 +1,8 @@
 import asyncio
 from app.core.database import engine, Base
 
+metadata = sqlalchemy.MetaData()
+
 async def init():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)

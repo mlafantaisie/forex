780b075717b5b6fb6940569e556d8e163e0e310a
Matthew <74075600+mlafantaisie@users.noreply.github.com>
2025-06-09 13:52:09 -0400

    Delete app/models/users.py

--- app/models/users.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from app.core.database import Base
-
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(50), unique=True, nullable=False)
-    password_hash = Column(String(128), nullable=False)
